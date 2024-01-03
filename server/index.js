c8a886feac9036abe735ea1e6638363a92e98b73
Karthikkv1 <karthik.k.v.036@gmail.com>
2024-01-03 15:58:40 +0530

    Server created

---
+++ server/index.js
@@ -0,0 +1,11 @@
+// File  created on 03-01-2024
+
+import express from 'express';  // importing express 03-01-2023
+
+const app = express(); // To initialise express js
+
+const PORT =8000;
+
+app.listen(PORT,() => console.log(`server is running successfully on ${PORT} `))  // To use express server 03-01-2023
+
+
