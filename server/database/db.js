0fa032f99e60f1bf829fb508fa245b74bd13e3d8
Karthikkv1 <karthik.k.v.036@gmail.com>
2024-01-06 15:12:13 +0530

    error

--- server/database/db.js
+++ server/database/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"; // 04-01-2024
 
 export const Connection = async () => {
   const URL =
-    "mongodb://Karthik:<Karthikswebsite>@ac-yvtti9e-shard-00-00.kxv1o8i.mongodb.net:27017,ac-yvtti9e-shard-00-01.kxv1o8i.mongodb.net:27017,ac-yvtti9e-shard-00-02.kxv1o8i.mongodb.net:27017/FLIPKART CLONE?ssl=true&replicaSet=atlas-2454tz-shard-0&authSource=admin&retryWrites=true&w=majority";
+    "mongodb+srv://dbm135338:<zy25AaqyIQ8vBvzm>@ecommerce.5fmujji.mongodb.net/?retryWrites=false&w=majority";
   try {
     await mongoose.connect(URL, {
       useUnifiedTopology: true,
--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@
 
 import express from 'express';  // importing express 03-01-2023
 
-import Connection from './database/.js';    //DataBase connection file 04-01-2024
+import Connection from './database/db.js';    //DataBase connection file 04-01-2024
 
 const app = express(); // To initialise express js
 
