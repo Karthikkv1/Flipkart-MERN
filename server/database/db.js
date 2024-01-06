c80179449b951521791291428f0bee87a64460df
Karthikkv1 <karthik.k.v.036@gmail.com>
2024-01-06 22:37:45 +0530

    Error solved

--- server/database/db.js
+++ server/database/db.js
@@ -1,13 +1,11 @@
 import mongoose from "mongoose"; // 04-01-2024
 
+
 export const Connection = async () => {
   const URL =
-    "mongodb+srv://dbm135338:<zy25AaqyIQ8vBvzm>@ecommerce.5fmujji.mongodb.net/?retryWrites=false&w=majority";
+    "mongodb+srv://dbm135338:zy25AaqyIQ8vBvzm@ecommerce.5fmujji.mongodb.net/ PROJECT 0?retryWrites=false&w=majority"; //Database connection 06-01-2024
   try {
-    await mongoose.connect(URL, {
-      useUnifiedTopology: true,
-      useNewUrlParser: true,
-    });
+    await mongoose.connect(URL, {useUnifiedTopology: true,useNewUrlParser: true,});
     console.log("Database connected successfully");
   } catch (error) {
     console.log("Error while connecting with the database", error.message);
