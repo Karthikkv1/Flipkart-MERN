9ccd7c1026f51e6543b6bd989707ab4c0b4d1ce5
Karthikkv1 <karthik.k.v.036@gmail.com>
2024-02-28 15:28:01 +0530

    Login 2

--- client/src/components/header/CustomButtons.jsx
+++ client/src/components/header/CustomButtons.jsx
@@ -1,54 +1,65 @@
+import { useState } from "react"; //28-02-2024
 
-import {Box,Button, Typography,styled} from '@mui/material';
+import { Box, Button, Typography, styled } from "@mui/material";
 
-import {ShoppingCart} from '@mui/icons-material';
+import { ShoppingCart } from "@mui/icons-material";
 
-const Wrapper =styled(Box)
-`
-display:flex;
-margin:0 3% 0 auto;
- & > button, & > p, & > div {
+//Components 28-02-2024
+import LoginDialog from "../login/LoginDialog";
+
+const Wrapper = styled(Box)`
+  display: flex;
+  margin: 0 3% 0 auto;
+  & > button,
+  & > p,
+  & > div {
     margin-right: 40px;
     font-size: 14px;
-    align-items:center;
-
- }
-
-`
-
-const Container= styled(Box)
-`
-display:flex;
+    align-items: center;
+  }
 `;
 
-const LoginButton = styled(Button)
-`
-color: #2874f0;
-background:#FFFFFF;
-text-transform: none;
-padding:5px 40px;
-border-radius:2px;
-box-shadow:none;
-font-weight:600;
-height:32px;
-
+const Container = styled(Box)`
+  display: flex;
 `;
 
-const CustomButtons =() =>{
-    return(
-        <Wrapper>
-            
-            <LoginButton variant='contained'>Login</LoginButton>
+const LoginButton = styled(Button)`
+  color: #2874f0;
+  background: #ffffff;
+  text-transform: none;
+  padding: 5px 40px;
+  border-radius: 2px;
+  box-shadow: none;
+  font-weight: 600;
+  height: 32px;
+`;
 
-            <Typography style={{marginTop:3, width:135}}>Become a Seller</Typography>
-            <Typography style={{marginTop:3}}>More</Typography>
+const CustomButtons = () => {
+  const [open, setOpen] = useState(false);
 
-            <Container>
-                <ShoppingCart/>
-                <Typography>Cart</Typography>
-            </Container>
-        </Wrapper>
-    )
-}
+  const openDialog = ()  =>
+  {
+    setOpen(true);
+
+  }
+  return (
+    <Wrapper>
+      <LoginButton variant="contained" onClick={() => openDialog()}>
+        Login
+      </LoginButton>
+
+      <Typography style={{ marginTop: 3, width: 135 }}>
+        Become a Seller
+      </Typography>
+      <Typography style={{ marginTop: 3 }}>More</Typography>
+
+      <Container>
+        <ShoppingCart />
+        <Typography>Cart</Typography>
+      </Container>
+      <LoginDialog open ={open} setOpen={setOpen} />
+    </Wrapper>
+  );
+};
 
 export default CustomButtons;
--- client/src/components/login/LoginDialog.jsx
+++ client/src/components/login/LoginDialog.jsx
@@ -1,9 +1,38 @@
-import { Dialog } from '@mui/material';
+import { Dialog , Box ,TextField,Typography ,Button , styled} from "@mui/material";
 
-const LoginDialog = () => {
+
+const   Component = styled(Box) `
+height:70vh;
+width:90vh;
+`;
+
+const Image = styled(Box)`
+background:#2874f0 url(https://static-assets-web.flixcart.com/www/linchpin/fk-cp-zion/img/login_img_c4a81e.png) center 85% no-repeat;
+height:100%;
+width:40%;
+`;
+const LoginDialog = ({open , setOpen}) => {
+    const handleClose =() =>{
+        setOpen(false);
+    }
   return (
-  <Dialog open={true}>Hello</Dialog>
-  )
-};
+     <Dialog open={open} onClose={handleClose}>
+        <Component>
+            <Box style={{display:'flex',height:'100%'}}>
+            <Image><Typography variant="h5"></Typography></Image>
+            <Box>
+            <TextField variant="standard" label="Enter Email/Mobile Number"/>
+            <TextField variant="standard" label="Enter Password"/>
+            <TextField>By continuin , you are agree to Flipkart's Terms of use and privacy Policy</TextField>
+            <Button>Login</Button>
+            <Typography>OR</Typography>
+            <Button>Request Otp</Button>
+            <Typography>New to Flipkart? Create an account</Typography>
+                </Box>
+                </Box>
+        </Component>
+        </Dialog>
+     )
+}
 
 export default LoginDialog;
