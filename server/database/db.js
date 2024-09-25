894c884def11b3a7cd875da53d2b2c865519bd4a
KARTHIK K V <154204804+Karthikkv1@users.noreply.github.com>
2024-09-25 12:02:40 +0530

    Update db.js

--- server/database/db.js
+++ server/database/db.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose"; // 04-01-2024
 
 export const Connection = async (username, password) => {
   const URL =
-    `mongodb+srv://${username}:${password}@ecommerce.5fmujji.mongodb.net/PROJECT0?retryWrites=false&w=majority`; //Database connection 06-01-2024
+    `mongodb+srv://${username}:${password}/`; //Database connection 06-01-2024
   try {
     await mongoose.connect(URL, {useUnifiedTopology: true,useNewUrlParser: true,});
     console.log("Database connected successfully");
